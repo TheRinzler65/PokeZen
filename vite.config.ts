3bc94432f212ee163f3203756448aee8c6116817
TheRinzler65 <leo.gruet65@gmail.com>
2026-05-14 03:48:49 +0200

    install package + config project

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [{ enforce: "pre", ...mdx() }, react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
